97261205ffce21f68a6ba6f0d29d86f1a9a08e12
NeboMilic <nebo.milic@gmail.com>
2018-02-07 16:46:55 +0100

    added config files for api key

---
+++ src/client/config/apiKey.js
@@ -0,0 +1,3 @@
+export default {
+    API_KEY: '<API KEY GOES HERE>' //GET YOUR KEY HERE http://omdbapi.com/apikey.aspx
+}; 
---
+++ src/client/config/index.js
@@ -0,0 +1,5 @@
+import apiKey from './apiKey';
+
+export default {
+    API_KEY: apiKey.API_KEY
+};
